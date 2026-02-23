39b19e22e63e8327d954a2d9f2fccd3673276328
zhinan <lznpojo@163.com>
2026-02-23 16:24:51 +0800

    init update

---
+++ src/main/java/com/support/server/supportrosterserver/SupportRosterServerApplication.java
@@ -0,0 +1,13 @@
+package com.support.server.supportrosterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportRosterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportRosterServerApplication.class, args);
+    }
+
+}
