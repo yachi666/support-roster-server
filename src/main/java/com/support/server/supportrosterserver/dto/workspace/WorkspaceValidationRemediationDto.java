d8d2827273dcfa85cab100673f7c43992519b572
zhinan <lznpojo@163.com>
2026-03-27 22:12:49 +0800

    Fix validation remediation issue identity

--- src/main/java/com/support/server/supportrosterserver/dto/workspace/WorkspaceValidationRemediationDto.java
+++ src/main/java/com/support/server/supportrosterserver/dto/workspace/WorkspaceValidationRemediationDto.java
@@ -3,6 +3,8 @@ package com.support.server.supportrosterserver.dto.workspace;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import tools.jackson.databind.annotation.JsonSerialize;
+import tools.jackson.databind.ser.std.ToStringSerializer;
 
 @Data
 @NoArgsConstructor
@@ -14,4 +16,6 @@ public class WorkspaceValidationRemediationDto {
     private String requiresRole;
     private Boolean previewable;
     private Boolean destructive;
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long recordId;
 }
